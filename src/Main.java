9a1de2c06eb5b83bf19993af71749dd40605eff9
3dartix <info@3dartix.ru>
2019-02-23 14:28:54 +0200

    в

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! Hello GitHub");
+
+        System.out.println("Now i try to combine this branch");
     }
 }
