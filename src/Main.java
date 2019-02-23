5331d1b3eb9e46da49b1188b5ec9868eed219cdc
3dartix <info@3dartix.ru>
2019-02-23 13:54:23 +0200

    новая ветка

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! Hello GitHub");
+
+        System.out.println("This is my first new branch to GitHub");
     }
+
 }
